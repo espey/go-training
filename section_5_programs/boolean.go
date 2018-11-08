1a9cb7718d65fd105bd8822aea315234ec60b36e
espey <erinpolsfoot@gmail.com>
2018-11-08 10:05:44 -0700

    adding boolena program for section 5 training

---
+++ section_5_programs/boolean.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "fmt"
+)
+
+// x is of type bool. 
+// assign values to variables and x will hold type bool
+var x bool
+
+func main() {
+  fmt.Println(x)
+  x = true
+  fmt.Println(x)
+}
