acdda5fcfe14be8b29213bf903e8915efcd421b9
espey <erinpolsfoot@gmail.com>
2018-11-29 23:47:56 -0700

    added exercise

---
+++ section_8_exercises/ex9/ex9.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  favSport := "football"
+  switch favSport {
+  case "basketball":
+    fmt.Println("false")
+  case "football":
+    fmt.Println("true")
+  }
+
+}
